aa1a60a03f82e4f66aaa0c4257c978fe4d4e8898
flynnduism <flynnduism@gmail.com>
2019-10-15 13:00:57 -0700

    rebuilding site Tue Oct 15 13:00:57 PDT 2019

--- deploy.sh
+++ deploy.sh
@@ -17,4 +17,4 @@ git commit -m "$msg"
 
 # Push source and build repos.
 git push origin dev
-git subtree push --prefix=public git@github.com:oam-dev/oam-dev.github.io.git master
+git subtree push -f --prefix=public git@github.com:oam-dev/oam-dev.github.io.git master
